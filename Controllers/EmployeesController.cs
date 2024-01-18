cc4800771a33b88fd4bc9d259aaaa4e0cb2b139f
vishal <vishalvaje131@gmail.com>
2024-01-19 01:06:25 +0530

    Added

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+
+namespace ModelController2.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            //List<Employee> lstEmps = new List<Employee>();
+
+            //lstEmps.Add(new Employee { EmpNo = 1, Name = "Yash", Basic = 12345, DeptNo = 10 });
+            //lstEmps.Add(new Employee { EmpNo = 2, Name = "Lalit", Basic = 12345, DeptNo = 10 });
+            //lstEmps.Add(new Employee { EmpNo = 3, Name = "Saurabh", Basic = 12345, DeptNo = 10 });
+
+            //return View(lstEmps);
+            List<Employee> list = Employee.GetAllEmployees();
+            return View(list);
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+                return NotFound();
+            Employee emp = Employee.GetSingleEmployee(id.Value);
+            return View(emp);
+           // return View();
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       // public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Employee obj, IFormCollection collection, string Name)
+        {
+            try
+            {
+                //string EmpNo = collection["EmpNo"];
+                //string Name1 = collection["Name"];
+                //string Basic = collection["Basic"];
+                //string DeptNo = collection["DeptNo"];
+
+                Employee.Create(obj);
+                ViewBag.Message = "success!";
+                return View();
+                //return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Employee emp = Employee.GetSingleEmployee(id.Value);
+            return View(emp);
+            //return View();
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee obj)
+        {
+            try
+            {
+                Employee.Update(obj);
+               return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int? id,Employee emp)
+        {
+            Employee obj = Employee.GetSingleEmployee(id.Value);
+            return View(obj);
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int? id, IFormCollection collection)
+        {
+            try
+            {
+                Employee.Delete(id.Value);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ModelController2.Controllers
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+        public static Employee GetSingleEmployee(int EmpNo)
+        {
+            Employee? emp = new Employee();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Employees WHERE EmpNo = @a1";
+                cmd.Parameters.AddWithValue("@a1", EmpNo);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+
+                if (dr.Read())
+                {
+                    emp.EmpNo = EmpNo;
+                    emp.Name = dr.GetString("Name");
+                    emp.Basic = dr.GetDecimal("Basic");
+                    emp.DeptNo = dr.GetInt32("DeptNo");
+
+                }
+                else
+                {
+                    Console.WriteLine("Not Found");
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return emp;
+
+        }
+
+
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> lstEmps = new List<Employee>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees";
+
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                Employee obj;
+
+                while (dr.Read())
+                {
+                    obj = new Employee();
+                    obj.EmpNo = dr.GetInt32("EmpNo"); ;
+                    obj.Name = dr.GetString("Name");
+                    obj.Basic = dr.GetDecimal("Basic");
+                    obj.DeptNo = dr.GetInt32("DeptNo");
+                    lstEmps.Add(obj);
+                }
+
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return lstEmps;
+        }
+
+        public static void Update(Employee employee)
+        {
+           
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "UPDATE Employees SET Name = @Name, Basic = @Basic, DeptNo = @DeptNo WHERE EmpNo = @EmpNo";
+                             
+                cmd.Parameters.AddWithValue("@EmpNo", employee.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Basic", employee.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", employee.DeptNo);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+               throw;
+            }
+        }
+        public static void Create(Employee employee)
+        {
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Employees (EmpNo, Name, Basic, DeptNo) VALUES (@EmpNo, @Name, @Basic, @DeptNo)";
+
+                cmd.Parameters.AddWithValue("@EmpNo", employee.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Basic", employee.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", employee.DeptNo);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public static void Delete(int empno)
+        {
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Delete from Employees Where EmpNo = @EmpNo";
+
+                cmd.Parameters.AddWithValue("@EmpNo",empno);
+                
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+
+}
